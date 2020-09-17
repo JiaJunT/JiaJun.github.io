98091e1928c88d951e765b08c3cdcbf0aa81d205
Jia Jun Tsan <s10189491@connect.np.edu.sg>
2020-09-18 03:12:23 +0800

    re:languages

--- js/StoreData.js
+++ js/StoreData.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     $('#LanguageSet').change(function () {
         localStorage.setItem('SetLanguage', $(this).val());
-        $('#LanguageSet').value(localStorage.getItem('SetLanguage'));
     });
 });
